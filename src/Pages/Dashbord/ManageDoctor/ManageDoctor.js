d3796ef5a8b637b5cb34d5325ff15d66f5065964
Robin Hossain <robinhossain5261@gmail.com>
2022-12-01 08:01:37 +0600

    manage doctor done

--- src/Pages/Dashbord/ManageDoctor/ManageDoctor.js
+++ src/Pages/Dashbord/ManageDoctor/ManageDoctor.js
@@ -1,9 +1,72 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
 
 const ManageDoctor = () => {
+    const { data: doctors = [], isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h3 className="text-3xl mb-5">Manage Doctor</h3>
+            <h3 className="text-3xl mb-5">Manage Doctor : {doctors?.length}</h3>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Specilaty</th>
+                            <th>Email</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, i) =>
+                                <tr
+                                    key={doctor._id}
+                                >
+                                    <td>{i + 1}</td>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={doctor.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{doctor.name}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className='font-semibold'>{doctor.specialty}</td>
+                                    <td>{doctor.email}</td>
+                                    <td><button className="btn btn-error btn-xs">Delete</button> </td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
