e4afa0249b26a9981a1193ca09be62f9ae7d3c84
Robin Hossain <robinhossain5261@gmail.com>
2022-12-02 09:33:50 +0600

    Users delete done

--- src/Pages/Dashbord/AllUsers/AllUsers.js
+++ src/Pages/Dashbord/AllUsers/AllUsers.js
@@ -27,6 +27,25 @@ const AllUsers = () => {
                 }
             })
     }
+
+
+
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Delete successfully.');
+                    refetch();
+                }
+            })
+    }
+
     return (
         <div>
             <h2 className="text-3xl mb-5">All Users</h2>
@@ -50,7 +69,11 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user?.role !== 'admin' && <button onClick={() => handaleMakeAdmin(user._id)} className='btn btn-xs btn-secondary'>Admin</button>}</td>
-                                <td><button className="btn btn-xs btn-error">Delete</button></td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDelete(user._id)}
+                                        className="btn btn-xs btn-error">Delete</button>
+                                </td>
                             </tr>)
                         }
 
